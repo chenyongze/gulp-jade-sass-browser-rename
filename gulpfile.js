6a928f0abc9b16b5632d50a753d136f9e9115cbc
Yongze <sapphire.php@gmail.com>
2018-01-15 15:33:40 +0800

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var jade = require('gulp-jade');
+var reload = browserSync.reload;
+var sass = require('gulp-sass');
+var bs = require("browser-sync").create();
+var rename = require("gulp-rename");
+/**
+ * Compile jade files into HTML
+ */
+gulp.task('templates', function() {
+    return gulp.src('src/**/index.jade')
+        .pipe(jade({pretty: true}))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('dist/'))
+        
+});
+
+gulp.task('styles', () => {
+    return gulp.src('./src/**/*.scss')
+        .pipe(sass())
+        .pipe(rename('bundle.min.css'))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+})
+
+gulp.task('images', () => {
+    return gulp.src('./src/**/*.png')
+        .pipe(gulp.dest('dist'))
+})
+
+/**
+ * Important!!
+ * Separate task for the reaction to `.jade` files
+ */
+gulp.task('jade-watch', ['templates'], reload);
+gulp.task('styles-watch', ['styles'], reload);
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        },
+    })
+})
+/**
+ * Serve and watch the jade files for changes
+ */
+gulp.task('default', ['images', 'browserSync'], function() {
+    gulp.watch('src/**/*.scss', ['styles-watch']);
+    gulp.watch('src/**/*.jade', ['jade-watch']);
+});
